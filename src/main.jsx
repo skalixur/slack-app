875bdcb54641aa74d268d38365c30ec7e733f685
Skalixur <jmjmbo5@gmail.com>
2025-02-23 17:15:07 +0800

    Change base to root for subdomain

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <AuthContextProvider>
         <UserChannelProvider>
-          <BrowserRouter basename="/slack-app">
+          <BrowserRouter>
             <App />
             <Toaster />
           </BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/slack-app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
